51682747ef03668c66eb86e08279d9e63b594bf5
wegscd <Douglas_E_Wegscheid@whirlpool.com>
2015-11-15 10:13:48 -0500

    Fix popups to work with both Linux and Windows.

--- FRC3620_Timeclock/src/org/frc3620/timeclock/gui/TimeclockFrame.java
+++ FRC3620_Timeclock/src/org/frc3620/timeclock/gui/TimeclockFrame.java
@@ -60,23 +60,14 @@ public class TimeclockFrame extends javax.swing.JFrame {
         worksessionTable.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseReleased(MouseEvent e) {
-                if (!isMentorMode()) {
-                    return;
-                }
-                int r = worksessionTable.rowAtPoint(e.getPoint());
-                if (r >= 0 && r < worksessionTable.getRowCount()) {
-                    worksessionTable.setRowSelectionInterval(r, r);
-                } else {
-                    worksessionTable.clearSelection();
-                }
-
-                int rowindex = worksessionTable.getSelectedRow();
-                if (rowindex < 0) {
-                    return;
-                }
-                if (e.isPopupTrigger() && e.getComponent() instanceof JTable) {
-                    worksessionTablePopupMenu.show(e.getComponent(), e.getX(), e.getY());
-                }
+                logger.debug ("mouseReleasedEvent {}", e);
+                worksessionPopup(e);
+            }
+            
+            @Override
+            public void mousePressed(MouseEvent e) {
+                logger.debug ("mousePressedEvent {}", e);
+                worksessionPopup(e);
             }
         });
 
@@ -143,6 +134,27 @@ public class TimeclockFrame extends javax.swing.JFrame {
     public boolean isWindowClosing() {
         return windowClosing;
     }
+    
+    void worksessionPopup(MouseEvent e) {
+        if (!isMentorMode()) {
+            return;
+        }
+        int r = worksessionTable.rowAtPoint(e.getPoint());
+        if (r >= 0 && r < worksessionTable.getRowCount()) {
+            worksessionTable.setRowSelectionInterval(r, r);
+        } else {
+            worksessionTable.clearSelection();
+        }
+
+        int rowindex = worksessionTable.getSelectedRow();
+        if (rowindex < 0) {
+            return;
+        }
+        if (e.isPopupTrigger() && e.getComponent() instanceof JTable) {
+            worksessionTablePopupMenu.show(e.getComponent(), e.getX(), e.getY());
+        }
+    }
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
